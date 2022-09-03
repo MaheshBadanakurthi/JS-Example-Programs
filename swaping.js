8a9737570d23bb562c10285f985fb5331d33134a
Mahesh <itstribe.mahi@gmail.com>
2022-09-03 15:26:00 +0530

    swapping

---
+++ swaping.js
@@ -0,0 +1,14 @@
+let x="Mahesh";
+let y="cgcs"
+x=y;
+temp=x;
+y=x;
+console.log(x);
+console.log(y);
+
+
+// let a=10;
+// let b=12;
+// [a,b]=[b,a]  // destructuring 
+// console.log(a);
+// console.log(b);
