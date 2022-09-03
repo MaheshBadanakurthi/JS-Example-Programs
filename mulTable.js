1cc91736f7b1b8b57d68e6c6d92bd852020ff32d
Mahesh <itstribe.mahi@gmail.com>
2022-09-03 15:19:15 +0530

    multiplication of a number

---
+++ mulTable.js
@@ -0,0 +1,9 @@
+let num=5;
+for(i=1;i<=10;i++){
+    let val=num*i;
+    let result= `${num} * ${i} = ${val}`;
+    console.log(result);
+    // if(i==5){// without it it execute all
+    //     break;
+    // }
+}
