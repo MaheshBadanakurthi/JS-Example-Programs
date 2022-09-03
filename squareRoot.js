51ca800371284fc021b4a145e2d99108aaa780ea
Mahesh <itstribe.mahi@gmail.com>
2022-09-03 15:22:43 +0530

    square root of a number

---
+++ squareRoot.js
@@ -0,0 +1,8 @@
+let sq=[121.255,25,36,144,989,65];
+
+for (let i of sq){
+
+        let value=Math.sqrt(i);
+         console.log(value);
+         console.log(Math.sqrt('Mahesh'));// gives a NaN.
+}
