e8ee6d8f11678e9949fc3edbdf2f0a2ab0569a8e
Mahesh <itstribe.mahi@gmail.com>
2022-09-03 15:23:38 +0530

    Sum of numbers

---
+++ sumOfNaturals.js
@@ -0,0 +1,17 @@
+// let num=4;
+// let sum=0;
+// for (i=0;i<=num;i++){
+//     sum=sum+i;
+// }
+// console.log(sum)
+
+
+
+let num1=7;
+let sum=0;
+let i=1;
+while(i<=num1){
+    sum+=i;
+    i++;
+}
+console.log(sum);
