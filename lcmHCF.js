3aafb23be3be63b774760ee2036501e1b068cfd8
Mahesh <itstribe.mahi@gmail.com>
2022-09-03 15:17:38 +0530

    lcm of numbers

---
+++ lcmHCF.js
@@ -0,0 +1,13 @@
+// let num1=24;
+// let num2=120;
+// let hcf;
+// for (i=1;i<=num1 && num2;i++){
+//     if(num1%i==0 && num2%i==0 ){
+//         hcf=i;
+//     }
+// }
+// console.log(hcf);
+
+
+
+
