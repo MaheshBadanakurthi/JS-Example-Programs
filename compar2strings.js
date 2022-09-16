629c026e1231b19435e98a0efa56cf2008c851ea
Mahesh <itstribe.mahi@gmail.com>
2022-09-16 22:33:31 +0530

    two strings are comparing

---
+++ compar2strings.js
@@ -0,0 +1,16 @@
+// her we are comparing two strings
+
+function twoStrings(str1,str2){
+    let x=str1.toUpperCase();
+    let y=str2.toUpperCase();
+    if(x===y){
+        console.log("Two strings ar eequall")
+    }
+    else
+    console.log("Not equal ")
+}
+let a='Am learning Web dev'
+let b='am learning WEB dev'
+twoStrings(a,b)
+
+
