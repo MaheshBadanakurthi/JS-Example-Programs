322b328e39392a819c622a54cd22e5dd61e92278
Mahesh <itstribe.mahi@gmail.com>
2022-09-16 22:38:25 +0530

    string and substring

---
+++ stringSubstring.js
@@ -0,0 +1,15 @@
+// here  we will see if a string is containing another string
+
+function str(value){
+    let ourValue="Any"// case sensitive
+    if(value.includes(ourValue)){
+        console.log("yes it has our value")
+    }
+    else
+    console.log("Nooooooo ")
+}
+let string=" thing Any happens at  time"
+str(string)
+
+
+
