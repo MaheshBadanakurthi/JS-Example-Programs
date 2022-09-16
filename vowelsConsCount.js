11938d8cbc63df5559a3bff40183eb2d0e458b7f
Mahesh <itstribe.mahi@gmail.com>
2022-09-16 22:32:04 +0530

    Vowels and Consonents Count

---
+++ vowelsConsCount.js
@@ -0,0 +1,16 @@
+
+const VowelList=['a','e','i','o','u'];
+function VList(str){
+    let count=0;
+    let Vowels=[]
+    let cons=[]
+    for(letter of str.toLowerCase()){
+        if(VowelList.includes(letter)){
+            Vowels.push(letter);
+        }
+        else cons.push(letter);
+    }
+    console.log(`vowels are ${Vowels} count is ${Vowels.length}`)
+    console.log( ` consonents are ${cons} count ${cons.length}`)
+}
+VList('mahesh is not so special')
