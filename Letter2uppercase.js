0b33a3543126a8f81f8e1c0d1bad521a1b5e3e21
Mahesh <itstribe.mahi@gmail.com>
2022-09-16 22:27:45 +0530

    Letter to upper case

--- Letter2uppercase.js
+++ Letter2uppercase.js
@@ -8,10 +8,22 @@
 // letter2upper('bmahesh')
 
 //another way
-function str2Upper(str){
-    let string=str.charAt(0).toUpperCase()+str.slice(1)
-    console.log(string)
+// function str2Upper(str){
+//     let string=str.charAt(0).toUpperCase()+str.slice(1)// here slice(1) gives array from 1st elements  if we give 2 it gives from 2nd element
+//     console.log(string)
+// }
+// str2Upper('mahesh')
+
+function string(str){
+    let x=str.split('')
+    let y=x[0].toUpperCase();
+    
+
 }
-str2Upper('mahesh')
+const nam='mahesh'
+string(nam)
+
+
+
 
 
