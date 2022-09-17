da68e34df70451110fbf25372211f0a1f84a8f80
Mahesh <itstribe.mahi@gmail.com>
2022-09-17 15:19:50 +0530

    String with another string

---
+++ strWithanother.js
@@ -0,0 +1,15 @@
+// we are checking a string starts with other or our given string
+function string(str){
+    let checkingString='this';
+    if(str.startsWith(checkingString)){
+        console.log("yes starting with ")
+    }
+    else
+    console.log("noooooo  ")
+}
+let sentense="This sentence id about our learning";
+string(sentense);
+
+
+
+
