2d09a3c5a06a791f5c07dc95f79994affbc2cc8d
Mahesh <itstribe.mahi@gmail.com>
2022-09-17 15:14:08 +0530

    Leap year

---
+++ leapYear.js
@@ -0,0 +1,11 @@
+// checking given year is leap year or not
+function Leapyear(num){
+    if(num%4==0 && num%100 !=0 ||  num%400==0){
+        console.log("its a leap year")
+    }
+    else
+    console.log("not a leap")
+}
+Leapyear(1645)
+
+
