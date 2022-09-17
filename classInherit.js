f11c93a4f451e91cf0d136c435b54efaae21b067
Mahesh <itstribe.mahi@gmail.com>
2022-09-18 00:50:14 +0530

    Dinheritance of a class

--- classInherit.js
+++ classInherit.js
@@ -1,7 +1,7 @@
 class Anyname{
-    constructor(nam,place,place2){
-
-        this.nam=nam;
+    constructor(name,place,place2){
+// here it is not necessary to write same as keys . it works based on the order preference
+        this.name=name;// here name should be matched with constractor key words
         this.place=place;
         this.place2=place2;
         
@@ -10,7 +10,7 @@ class Anyname{
     }
     class secondClass extends Anyname{
         constructor(name,place,place2,gender){
-            super(name,place,place2,gender);
+            super(name,place,place2,gender); // by using super we calling that parent values inside to the child
             this.gender=gender;
         }
 
