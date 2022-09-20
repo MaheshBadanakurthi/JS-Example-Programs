332844a75c791514abeb12b2e89abb274ce9d123
Mahesh <itstribe.mahi@gmail.com>
2022-09-20 22:43:15 +0530

    Date and time

--- dateTime.js
+++ dateTime.js
@@ -2,7 +2,7 @@ let date=new Date()
 console.log(date)
 // console.log(Date.now())
 console.log(date.getFullYear()) //2022
-// console.log(date.getDay())
+console.log(date.getDay())
 // console.log(date.getHours())
 
 // console.log(date.setFullYear(2020))
@@ -14,7 +14,7 @@ let year=date.getFullYear();
 console.log(day+"-"+month+"-"+year)
 console.log(day+"@"+month+"@"+year)
 console.log(day+"/"+month+"/"+year)
-
+// below is used to print current date and time
 const time = date.toLocaleTimeString();// for current time
 console.log(time)
 console.log(date.getHours()+"-"+date.getMinutes()+"-"+date.getMilliseconds())
